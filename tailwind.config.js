d6c1211382718a9a0d0a2d9005ff0372dc3ba537
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-07-13 23:13:57 -0400

    tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// It's the configuration file that tells Tailwind how to behave when it compiles your styles.
+
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class", // class strategy for manual toggling
