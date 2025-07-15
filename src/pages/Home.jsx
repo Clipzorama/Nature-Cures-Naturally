3a148ca35b6feaced435d77fd771d34de74a14ad
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-07-14 22:20:43 -0400

    made some major fixes to continue project. also enabled my tailwind classes, utilities and animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import { NavBar } from '@/components/Navbar'
 
 function Home() {
     return (
-        <>
+        <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
             {/* Navbar  */}
             <NavBar />
-        </>
+        </div>
     );
 }
 
