470705eda81a85ef138efc37d0076fa972668bd9
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-07-27 22:39:16 -0400

    implemented the hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import { NavBar } from '@/components/Navbar'
-import { LightSwitch } from '@/components/LightSwitch';
+import { LightSwitch } from '@/components/LightSwitch'
+import { HeroSide } from '@/components/HeroSide';
 
 function Home() {
     return (
         <div className='min-h-screen bg-background text-primary overflow-x-hidden'>
             {/* Navbar  */}
             <LightSwitch className='fixed hidden md:block top-20 right-6 z-50 p-2'/>
-            
             <NavBar />
+            <HeroSide />
         </div>
     );
 }
