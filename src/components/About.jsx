7838642e8c05a9227fb13b1d9f0ee1abdd8937c7
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-07-29 17:25:31 -0400

    added about component

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+export const AboutSection = () => {
+
+
+
+    return (
+        <section id="about" className="flex flex-col items-center justify-center px-4 relative min-h-screen">
+            <div className="container flex justify-center items-center">
+                <div className="flex flex-col ">
+                    <h2>Hello I am Nicole Wynaar</h2>
+
+                </div>
+            </div>
+
+            
+        </section>
+
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { NavBar } from '@/components/Navbar'
 import { LightSwitch } from '@/components/LightSwitch'
 import { HeroSide } from '@/components/HeroSide';
+import { AboutSection } from '@/components/About';
 
 function Home() {
     return (
@@ -9,6 +10,7 @@ function Home() {
             <LightSwitch className='fixed hidden md:block top-30 right-6 z-50 p-2'/>
             <NavBar />
             <HeroSide />
+            <AboutSection />
         </div>
     );
 }
