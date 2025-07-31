c5919ff4dce46d63682b92114ff55e1de9796b66
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-07-31 00:06:37 -0400

    some improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { AboutSection } from '@/components/About';
 function Home() {
     return (
         <div className='min-h-screen bg-background text-primary overflow-x-hidden'>
-            {/* Navbar  */}
+            
             <LightSwitch className='fixed hidden md:block top-30 right-6 z-50 p-2'/>
             <NavBar />
             <HeroSide />
