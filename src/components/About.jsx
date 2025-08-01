95287438c42bf53daf9fb7403e0831e225f163dd
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-07-31 21:03:59 -0400

    adding card concept and implementing lucide

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,54 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {
+  Leaf,
+  Apple,
+  Sprout,
+  Brain,
+  HeartPulse,
+  ClipboardList,
+  Droplet,
+  Salad,
+  Smile,
+  Utensils,
+  GraduationCap,
+  FileText,
+  NotebookPen,
+  BookOpenCheck,
+  Award,
+} from "lucide-react";
 
 
 gsap.registerPlugin(ScrollTrigger);
 
+// look for websites like "react bits" to find a cool background for the website
+
 export const AboutSection = () => {
+
+    const healing = [
+        {name: "Herbal Remedies", icon: Leaf},
+        {name: "Whole Foods Nutrition", icon: Apple},
+        {name: "Plant-Based Healing", icon: Sprout},
+        {name: "Mind-Body Connection", icon: Brain},
+        {name: "Emotional Wellbeing", icon: HeartPulse},
+    ]
+
+    const coaching = [
+        {name: "Meal Planning", icon: ClipboardList},
+        {name: "Detox Guidance", icon: Droplet},
+        {name: "Plant-Based Healing", icon: Salad},
+        {name: "Lifestyle Coaching", icon: Smile},
+        {name: "Mindful Eating", icon: Utensils},
+    ]
+
+    const certification = [
+        {name: "IIN Certified Coach", icon: GraduationCap},
+        {name: "Health History Assessment", icon: FileText},
+        {name: "Wellness Journals", icon: NotebookPen},
+        {name: "Organic Meal Guides", icon: BookOpenCheck},
+        {name: "Depression Studies Certificate", icon: Award},
+    ]
 
     const headingRef = useRef(null);
     const textRef = useRef(null);
@@ -57,20 +100,20 @@ export const AboutSection = () => {
     return (
         <section id="about" className="px-2 md:px-4 py-32 relative min-h-screen">
             <h1 ref={headingRef} className="text-center text-3xl md:text-4xl text-primary mb-20 font-bold">With hands in the soil and heart in the kitchen, I'm Nicole Wynaar!</h1>
-            <div className="container flex flex-col items-center md:items-start md:flex-row gap-10 md:gap-20">
+            <div className="container flex flex-col items-center lg:items-start lg:flex-row gap-10 md:gap-20">
 
                 {/* left side */}
                 <div className="flex flex-col w-[100%] md:w-[70%]" ref={textRef}>
                     <h2 className="text-primary text-3xl text-center">About Me</h2>
-                    <p className="text-pretty text-primary text-lg text-center mt-12 mb-8">
+                    <p className="text-pretty text-primary text-md md:text-lg text-center mt-12 mb-8">
                         Growing up in a family rooted in the land, I was raised on my mothers side of the family farm, where we grew our own fruits and vegetables. Everything we cooked came straight from the garden. 
                         From as early as five years old, I was taught not only how to cook but also how to plant and care for the food we ate. My grandparents passed down this knowledge with love and care, teaching me how to live in harmony with nature.
                     </p>
-                    <p className="text-pretty text-primary text-lg text-center mb-8">
+                    <p className="text-pretty text-primary text-md md:text-lg text-center mb-8">
                         When we were sick, whether it was a cold, a headache, or even more chronic issues like high blood pressure, we didn't run to the hospital. My grandmother believed in the power of nature and would make healing remedies using herbs from our own garden. 
                         She managed her diabetes and high blood pressure with natural remedies like garlic and ginger tea, and I was right there with her, learning every step of the way.
                     </p>
-                    <p className="text-pretty text-primary text-lg text-center mb-8">
+                    <p className="text-pretty text-primary text-md md:text-lg text-center mb-8">
                         I didn't just want to cook. I wanted to educate. I believe food is medicine, and what we eat should nourish and heal us. Our bodies are organic, and they thrive on what nature provides. 
                         Nature Curious Naturally is my way of reconnecting people to the healing wisdom of food and sharing how a natural, holistic lifestyle can support lifelong health and wellness.
                     </p>
@@ -89,6 +132,11 @@ export const AboutSection = () => {
                     <img className="w-full max-h-[750px] object-cover rounded-2xl" ref={imageRef} src={Nicole} alt="My Picture" />
 
                 </div>
+            </div>
+
+            {/* Clean card concept ill add here */}
+            <div>
+
             </div>
 
             
