7e007740d2aef0a481509c2a69efc5a239288de2
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-08-05 15:27:26 -0400

    implemented the leaf background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@ import { NavBar } from '@/components/Navbar'
 import { LightSwitch } from '@/components/LightSwitch'
 import { HeroSide } from '@/components/HeroSide';
 import { AboutSection } from '@/components/About';
+import LeafRain from "@/uimods/LeafRain";
 
 function Home() {
     return (
         <div className='min-h-screen bg-background text-primary overflow-x-hidden'>
-            
+            <LeafRain count={50} />
             <LightSwitch className='fixed hidden md:block top-30 right-6 z-50 p-2'/>
             <NavBar />
             <HeroSide />
