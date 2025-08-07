4bc872aa312b91eb6351635c2876ebdc578297c0
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-08-06 22:53:17 -0400

    Create Recipe.jsx

---
+++ src/components/Recipe.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+export const RecipeSection = () => {
+    return (
+        <section id="recipes" className="py-32 relative min-h-screen">
+            <h1  className="px-2 md:px-4 text-center text-3xl md:text-4xl text-card mb-20 font-bold">A Taste of My Food 🥣🥣</h1>
+            <div className="container">
+
+            </div>
+
+        </section>
+    );
+
+}
