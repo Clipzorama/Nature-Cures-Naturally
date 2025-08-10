8b3125d7d49aa6cdd75a8e4e501a0e9ff18b4731
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-08-09 22:16:14 -0400

    Create Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+
+
+
+export const ContactSection = () => {
+    return (
+        <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center text-card">
+                    Feel Free To Get In Touch
+                </h2>
+                <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                    Let's explore how we can create something impactful. I'm currently open to freelance and partnership opportunities.
+                </p>
+            </div>
+        </section>
+    );
+}
