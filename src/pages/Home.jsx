3ff2b92538a5ed978f1e6333a2bdbe0392396985
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-08-11 23:58:42 -0400

    changed theme placement for smaller screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
     return (
         <div className='min-h-screen bg-background text-primary overflow-x-hidden'>
             <LeafRain count={50} />
-            <LightSwitch className='fixed hidden md:block top-30 right-6 z-50 p-2'/>
+            <LightSwitch className='fixed hidden lg:block top-30 right-6 z-50 p-2'/>
             <NavBar />
             <HeroSide />
             <AboutSection />
