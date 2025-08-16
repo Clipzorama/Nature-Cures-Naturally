d24cb7936a966ba3745828f9e35dddf5cd0f197b
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-08-16 01:24:25 -0400

    dark theme default

--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -4,24 +4,24 @@ const ThemeContext = createContext();
 
 export const ThemeProvider = ({children}) => {
     
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState("dark");
 
     useEffect(() => {
         const stored = localStorage.getItem("theme");
-        if (stored === "dark") {
-            setTheme("dark");
-            document.documentElement.classList.add("dark");
+        if (stored === "light") {
+            setTheme("light");
+            document.documentElement.classList.add("light");
         } else {
             setTheme("light");
-            document.documentElement.classList.remove("dark");
+            document.documentElement.classList.remove("light");
         }
     }, [])
 
     const toggleTheme = () => {
-        const currentTheme = theme === "dark" ? "light" : "dark";
+        const currentTheme = theme === "light" ? "dark" : "light";
         setTheme(currentTheme);
         localStorage.setItem("theme", currentTheme);
-        document.documentElement.classList.toggle("dark")
+        document.documentElement.classList.toggle("light")
     }
 
     return (
